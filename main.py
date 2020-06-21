f6602703fc445a70a53ef890f6ffea7a3894d075
Orlando Ramirez <ramirez.orlando17@gmail.com>
2020-06-20 22:41:22 -0400

    Changes to main.py added three functions, you can manually enter a new client name

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
 clients = 'Pablo, Ricardo,'
 
+def create_client(client_name):
+    global clients   #you add the global clients so you can acces the clients global variable
+
+    clients += client_name
+    _add_comma()
+
+
+def list_clients():
+    global clients
+
+    print(clients)
+
+
+def _add_comma():
+    global clients
+
+    clients += ','
+
 
 if __name__ == '__main__':
-    clients += ' David'
-    print(clients)
+    list_clients()
+
+    create_client('David')
+
+    list_clients()
