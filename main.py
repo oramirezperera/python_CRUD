4e6d33b1317ff0651f5d87f4837b8b7b6ef5315c
Orlando Ramirez <ramirez.orlando17@gmail.com>
2020-06-26 22:00:26 -0400

    added the update function, compares the user input to the clients list, then replace the user clients list with the user input

--- main.py
+++ main.py
@@ -15,6 +15,15 @@ def list_clients():
     print(clients)
 
 
+def update_client(client_name, updated_client_name):
+    global clients
+
+    if client_name in clients:
+        clients = clients.replace(client_name + ',', updated_client_name + ',')
+    else:
+        print('Client is not in clients list')
+
+
 def _add_comma():
     global clients
 
@@ -26,19 +35,30 @@ def _print_welcome():
     print('*' * 50)
     print('What would you like to do today?')
     print('[C]reate client')
+    print('[U]pdate client')
     print('[D]elete client')
+
+
+def _get_client_name():
+    return input('What is the client name?')
 
 
 if __name__ == '__main__':
     _print_welcome()
 
     command = input()
+    command = command.upper()
 
     if command == 'C':
-        client_name = input('What is the client name?')
+        client_name = _get_client_name()
         create_client(client_name)
         list_clients()
     elif command == 'D':
         pass
+    elif command == 'U':
+        client_name = _get_client_name()
+        updated_client_name = input('What is the updated client name')
+        update_client(client_name, updated_client_name)
+        list_clients()
     else:
         print('invalid command')
