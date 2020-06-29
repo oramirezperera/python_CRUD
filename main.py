5a389063bb1a07e60c08d293b8439ab6564cd4cd
Orlando Ramirez <ramirez.orlando17@gmail.com>
2020-06-28 20:14:11 -0400

    Changes to the clients sistem now is not a sting, now is a list, updated all the function so they can work with lists.

--- main.py
+++ main.py
@@ -1,25 +1,28 @@
-clients = 'Pablo, Ricardo,'
+import sys
+
+clients = ['Pablo', 'Ricardo']
+
 
 def create_client(client_name):
     global clients   #you add the global clients so you can acces the clients global variable
 
     if client_name not in clients:
-        clients += client_name
-        _add_comma()
+        clients.append(client_name)
     else:
         print('Client already exist')
 
-def list_clients():
-    global clients
 
-    print(clients)
+def list_clients():
+    for idx, client in enumerate(clients):
+        print(f'{idx}: {client}')
 
 
 def update_client(client_name, updated_client_name):
     global clients
 
     if client_name in clients:
-        clients = clients.replace(client_name + ',', updated_client_name + ',')
+        index = clients.index(client_name)
+        clients[index] = updated_client_name
     else:
         print('Client is not in clients list')
 
@@ -28,25 +31,18 @@ def delete_client(client_name):
     global clients
 
     if client_name in clients:
-        clients = clients.replace(client_name + ',', '')
+        clients.remove(client_name)
     else:
         print('Client is not in clients list')
 
 
 def search_client(client_name):
-    clients_list = clients.split(',')
 
-    for client in clients_list:
+    for client in clients:
         if client != client_name:
             continue
         else:
             return True
-            
-
-def _add_comma():
-    global clients
-
-    clients += ','
 
 
 def _print_welcome():
@@ -54,12 +50,26 @@ def _print_welcome():
     print('*' * 50)
     print('What would you like to do today?')
     print('[C]reate client')
+    print('[L]ist clients')
     print('[U]pdate client')
     print('[D]elete client')
     print('[S]earch client')
 
+
 def _get_client_name():
-    return input('What is the client name?')
+    client_name = None
+
+    while not client_name:
+        client_name = input('What is the client name?')
+    
+        if client_name == 'exit':
+            client_name == None
+            break
+
+    if not client_name:
+        sys.exit()
+
+    return client_name
 
 
 if __name__ == '__main__':
@@ -89,5 +99,7 @@ if __name__ == '__main__':
             print('The client is in the client\'s list')
         else:
             print(f'The client {client_name} is not in our client\'s list')
+    elif command == 'L':
+        list_clients()
     else:
         print('invalid command')
